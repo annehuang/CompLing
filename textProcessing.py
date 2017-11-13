40cf4caf5230d54c135c7b0e909290ceb8329a3a
Anne Huang <ahuang27@uic.edu>
2017-11-12 20:25:24 -0600

    topics

--- textProcessing.py
+++ textProcessing.py
@@ -1,23 +1,30 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy
 import lda
+
 def main():
-    filename = "femaleItalianOutput.txt"
+    produceTopics("femaleItalian")
+    produceTopics("maleItalian")
+
+def produceTopics(filen):
+    filename = filen + "Output.txt"
 
     f = open(filename, "r").read()
     count_vect = CountVectorizer()
     data = [f]
-    arrFemale = count_vect.fit_transform(data)
-    #f = open("maleItalianOutput.txt, "r").read()
-    #count_vect = CountVectorizer()
-
-    #arrMale = count_vect.fit_transform(f)
+    arr = count_vect.fit_transform(data)
 
     model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
-    model.fit(arrFemale)  # model.fit_transform(X) is also available
-    topic_word = model.topic_word_  # model.components_ also works
+    model.fit(arr)
+    topic_word = model.topic_word_
     n_top_words = 8
 
+    out = ""
     for i, topic_dist in enumerate(topic_word):
         topic_words = numpy.array(list(count_vect.vocabulary_.keys()))[numpy.argsort(topic_dist)][:-n_top_words:-1]
-        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+        out += 'Topic {}: {}'.format(i, ' '.join(topic_words))
+        out += '\n'
+
+    f2 = open(filen + "Topics.txt", "w")
+    f2.write(out)
+    f2.close()
